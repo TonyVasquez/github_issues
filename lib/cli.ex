e9e31f4715cd2c5f817daebbcad9a18aba39ee22
Raul <tonyvasquiez@gmail.com>
2020-01-21 20:06:46 +0700

    init

---
+++ lib/cli.ex
@@ -0,0 +1,95 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch
+  to the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  @spec main(list()) :: no_return
+
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts("""
+      usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_info_descending_order()
+    |> last(count)
+    |> select_fields()
+    |> Scribe.inspect()
+  end
+
+  def select_fields(list) do
+    list
+    |> Enum.map(&table_struct/1)
+  end
+
+  defp table_struct(issue) do
+    %{
+      "#" => issue["number"],
+      "created_at" => issue["created_at"],
+      "title" => issue["title"]
+    }
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  def sort_info_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["create_at"] >= i2["create_at"]
+    end)
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  # bad arg or --help
+  def args_to_internal_representation(_), do: :help
+end
